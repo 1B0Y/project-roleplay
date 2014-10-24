3ac654c43b7e21a9cca2d7abc1ecd2e2440bdbd4
britishjackj@gmail.com <britishjackj@gmail.com@9318ff57-b472-33fd-257b-0f0ced4443b5>
2014-10-24 17:31:58 +0000

    - Changed the directory naming to make it "not branded". - Trashed accounts system and making it based on MTA accounts (since everyone doesn't have a MySQL database on hand).

---
+++ trunk/accounts/accounts_server.lua
@@ -0,0 +1,30 @@
+--[[
+	Project: SourceMode
+	Version: 1.0
+	Last Edited: 23/10/2014 (Jack)
+	Authors: Jack
+]]--
+
+-- Let's start off by adding our custom events that we'll need for later.
+addEvent("onPlayerAttemptLogin",true)
+addEvent("onPlayerAttemptRegister",true)
+addEvent("onPlayerAttemptRecovery",true)
+
+-- Setup some variables and tables we'll need.
+local REGISTER_WAITTIME = 60 -- Registration limit between account registrations (to prevent players making multiple accounts quickly).
+
+-- Setup some pre-definers since we'll be using custom functions
+local _logIn = logIn
+
+function logIn(username,password,encrypted,remember)
+	--Handle the login crap here
+end
+addEventHandler("rp
+
+function register(username,password,email)
+	--Handle the register crap here
+end
+
+function recover(username,email)
+	--Handle recovery crap here
+end
