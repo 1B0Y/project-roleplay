9956c67df3a94be59923ee897d2c88a9d7f35aec
britishjackj@gmail.com <britishjackj@gmail.com@9318ff57-b472-33fd-257b-0f0ced4443b5>
2014-10-25 18:57:22 +0000

    [Update] Added the first parts of the login stages (and available for testing!) (Jack) [NOTE] You can do /dReg username password to test the login panel.

--- trunk/accounts/accounts_server.lua
+++ trunk/accounts/accounts_server.lua
@@ -17,7 +17,30 @@ local REGISTER_WAITTIME = 60 -- Registration limit between account registrations
 local _logIn = logIn
 
 function logIn(username,password,encrypted,remember)
-	--Handle the login crap here + ban handling
+	--Check if we have a database connection
+	if not (exports.database:getConnection()) then
+		triggerClientEvent(client,"returnLoginStatus",client,"A networking issue has occured. Please contact an admin. (Err: #01)")
+		return
+	end
+	local connection = exports.database:getConnection()
+
+	--Check if the username, password and etc was passed through.
+	if (username) and (password) and (type(encrypted) == "boolean") and (remember) then
+		local username = username:lower()
+		
+		--Do username check and login stages within a callback function.
+		local query = dbQuery(
+			function(query,player)
+				local results = dbPoll(query,-1)
+				if results and type(results) == "table" and #results >= 1 then
+					triggerClientEvent(player,"returnLoginStatus",player,"Logged in as "..username..".")
+				else
+					triggerClientEvent(player,"returnLoginStatus",player,"The username "..username.." is not registered.")
+					return
+				end
+			end, {client}, connection, "SELECT * FROM accounts WHERE username=? LIMIT 1",username
+		)
+	end
 end
 addEventHandler("onPlayerAttemptLogin",root,logIn)
 
@@ -31,3 +54,16 @@ function recover(username,email)
 	--Handle recovery crap here
 end
 addEventHandler("onPlayerAttemptRecovery",root,recover)
+
+function tempReg(player,cmd,username,password)
+	if (username) and (#username >= 1) and (password) and (#password >= 1) then
+		local connection = exports.database:getConnection()
+		if (connection) then
+			dbExec(connection,"INSERT INTO accounts (username,password,lastlogin) VALUES (?,?,now())",tostring(username),tostring(password))
+			outputChatBox("Account inserted.")
+		else
+			error("No database connection.")
+		end
+	end
+end
+addCommandHandler("dReg",tempReg)
