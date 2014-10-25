5d712676b9b47d2048235200d3b43ea918db076d
britishjackj@gmail.com <britishjackj@gmail.com@9318ff57-b472-33fd-257b-0f0ced4443b5>
2014-10-25 02:19:46 +0000

    - Added "cache" storage feature to allow the accounts system to store data (such as username, password, ...) into that to be stored on the client's computer. (Jack) - Added a nice surprise for when you try to "Skip Login". (Jack) - Work will continue tomorrow / sunday.

--- accounts/accounts_client.lua
+++ accounts/accounts_client.lua
@@ -1,12 +1,12 @@
 --[[
 	Project: SourceMode
 	Version: 1.0
-	Last Edited: 23/10/2014 (Jack)
+	Last Edited: 25/10/2014 (Jack)
 	Authors: Jack
 ]]--
 
---Cache Data (do NOT touch as it contains client's account stuff)
-local cacheData
+local openedWindow -- Current opened window (For issue handling)
+local reset --Info status timer.
 
 --GUI stuff
 windows = {}
@@ -15,10 +15,13 @@ checkboxes = {}
 labels = {}
 buttons = {}
 
+local disabledButtons = {{"login-register"},{"login-recover"}}
+
 addEvent("onPlayerLoggedIn",true)
 
 function onStart()
 	local rX,rY = guiGetScreenSize() --Get the player's screen resolution (so we can make sure GUI fits in all resolutions)
+	loadCachedAccountData()
 	
 	--Login Panel
 	local width,height = 384,444
@@ -26,25 +29,30 @@ function onStart()
 	windows["login"] = guiCreateWindow(X,Y,width,height,"SourceMode - Login",false)
 		guiCreateStaticImage(17, 28, 350, 145,"images/mtalogo.png",false,windows["login"])
 		labels[1] = guiCreateLabel(119,181,147,18,"Username",false,windows["login"])
-		edits["login-username"] = guiCreateEdit(25,199,332,23,"",false,windows["login"])
+		edits["login-username"] = guiCreateEdit(25,199,332,23,getCachedData("username"),false,windows["login"])
 		labels[2] = guiCreateLabel(119,233,147,18,"Password",false,windows["login"])
-		edits["login-password"] = guiCreateEdit(25,251,332,23,"",false,windows["login"])
-		checkboxes["remember"] = guiCreateCheckBox(132, 278, 124, 21, "Remember Details", true, false, windows["login"])
-		buttons["login-login"] = guiCreateButton(27,299,330,26,"Login",false,windows["login"])
+		edits["login-password"] = guiCreateEdit(25,251,332,23,getCachedData("password"),false,windows["login"])
+		guiEditSetMasked(edits["login-password"],true)
+		checkboxes["remember"] = guiCreateCheckBox(132, 278, 124, 21, "Remember Details", getCachedData("remember"), false, windows["login"])
+		buttons["login-attemptLogin"] = guiCreateButton(27,299,330,26,"Login",false,windows["login"])
 		buttons["login-register"] = guiCreateButton(27,335,330,26,"Register an account",false,windows["login"])
-		buttons["login-recover"] = guiCreateButton(27,371,330,26,"Recover account",false,windows["login"])
+		buttons["login-recover"] = guiCreateButton(27,371,330,26,"Recover my account",false,windows["login"])
 		buttons["login-troll"] = guiCreateButton(287,418,87,16,"Skip Login",false,windows["login"]) --Comment this out if you're not the troll type ;)
-	labels["login-status"] = guiCreateLabel(29,398,328,20,"",false,windows["login"])
+		labels["login-status"] = guiCreateLabel(29,398,328,20,"",false,windows["login"])
+	addEventHandler("onClientGUIClick",buttons["login-attemptLogin"],onClick,false)
+	addEventHandler("onClientGUIClick",buttons["login-register"],onClick,false)
+	addEventHandler("onClientGUIClick",buttons["login-recover"],onClick,false)
+	addEventHandler("onClientGUIClick",buttons["login-troll"],onClick,false)
 	
 	--NOPE Panel--
 	local width,height = 416,228
 	local X,Y = exports.utils:getGUICenter(rX,rY,width,height)
 	windows["NOPE"] = guiCreateWindow(X,Y,width,height,"SourceMode - NOPE",false)
 		guiCreateStaticImage(128,23,165,98,"images/nope.jpg",false,windows["NOPE"])
-		labels[3] = guiCreateLabel(7,121,399,80,[[
-			You need an account to play, otherwise everything you do will be lost. We don't want that! :|\n\n
-			Register an account! it doesn't take too long."]],false,windows["NOPE"])
-	buttons["NOPE-fine"] = guiCreateButton(128,201,165,23,"hhh... fine!",false,windows["NOPE"])
+		labels[3] = guiCreateLabel(7,121,399,80,[[You need an account to play, otherwise everything you do will be lost. We don't want that! :|
+			Register an account! it doesn't take too long.]],false,windows["NOPE"])
+		buttons["NOPE-fine"] = guiCreateButton(128,201,165,23,"hhh... fine!",false,windows["NOPE"])
+	addEventHandler("onClientGUIClick",buttons["NOPE-fine"],onClick,false)
 	
 	local width,height = 493,382
 	local X,Y = exports.utils:getGUICenter(rX,rY,width,height)
@@ -53,18 +61,22 @@ function onStart()
 		edits["register-username"] = guiCreateEdit(27, 179, 270, 26, "", false, windows["register"])
 		labels[4] = guiCreateLabel(98, 161, 122, 18, "Username", false, windows["register"])
 		edits["register-password"] = guiCreateEdit(27, 223, 271, 27, "", false, windows["register"])
+		guiEditSetMasked(edits["register-password"],true)
 		labels[5] = guiCreateLabel(98, 205, 122, 18, "Password", false, windows["register"])
 		edits["register-confirm"] = guiCreateEdit(27, 268, 271, 24, "", false, windows["register"])
+		guiEditSetMasked(edits["register-confirm"],true)
 		labels[6] = guiCreateLabel(98, 250, 122, 18, "Confirm Password", false, windows["register"])
 		edits["register-email"] = guiCreateEdit(27, 314, 271, 24, "", false, windows["register"])
-		labels["register-email"] = guiCreateLabel(98, 296, 122, 18, "Email Address", false, windows["register"])
+		labels[12] = guiCreateLabel(98, 296, 122, 18, "Email Address", false, windows["register"])
 		labels["status-username"] = guiCreateLabel(335, 179, 144, 26, "Enter a username", false, windows["register"])
 		labels["status-password"] = guiCreateLabel(335, 223, 144, 27, "Enter a password", false, windows["register"])
 		labels["status-confirm"] = guiCreateLabel(335, 266, 144, 26, "Confirm your password", false, windows["register"])
 		labels["status-email"] = guiCreateLabel(335, 312, 144, 26, "Enter your email address", false, windows["register"])
 		labels[7] = guiCreateLabel(305, 172, 21, 168, "|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n|\n", false, windows["register"])
 		buttons["register-back"] = guiCreateButton(50, 350, 180, 22, "Go back to Log In", false, windows["register"])
-	buttons["register-attemptRegister"] = guiCreateButton(258, 350, 180, 22, "Register \"..username..\"", false, windows["register"])
+		buttons["register-attemptRegister"] = guiCreateButton(258, 350, 180, 22, "Register account", false, windows["register"])
+	addEventHandler("onClientGUIClick",buttons["register-back"],onClick,false)
+	addEventHandler("onClientGUIClick",buttons["register-attemptRegister"],onClick,false)
 	
 	local width,height = 384,284
 	local X,Y = exports.utils:getGUICenter(rX,rY,width,height)
@@ -78,48 +90,113 @@ function onStart()
 		labels[9] = guiCreateLabel(115, 162, 154, 14, "Email Address", false, windows["recovery"])
 		labels[10] = guiCreateLabel(115, 202, 154, 14, "Security Question", false, windows["recovery"])
 		buttons["recovery-back"] = guiCreateButton(34, 247, 136, 24, "Go back to Log In", false, windows["recovery"])
-	buttons["recovery-attemptRecover"] = guiCreateButton(205, 247, 136, 24, "Recover \"..username..\"", false, windows["recovery"])
-	
-	--Loop through all buttons and add eventhandlers
-	for k,v in ipairs(buttons) do
-		addEventHandler("onClientGUIClick",v,onClick,false)
-	end
-	
+		buttons["recovery-attemptRecovery"] = guiCreateButton(205, 247, 136, 24, "Recover account", false, windows["recovery"])
+	addEventHandler("onClientGUIClick",buttons["recovery-back"],onClick,false)
+	addEventHandler("onClientGUIClick",buttons["recovery-attemptRecovery"],onClick,false)
+
 	--Loop through all labels and add centering
 	for k,v in ipairs(labels) do
 		guiLabelSetHorizontalAlign(v, "center", true)
         guiLabelSetVerticalAlign(v, "center")
 	end
 	
+	--Loop through disabledButtons to turn off the features that aren't ready yet.
+	for k,v in ipairs(disabledButtons) do
+		guiSetEnabled(buttons[v[1]],false)
+	end
+	
+	openedWindow = "login"
 	guiSetVisible(windows["NOPE"],false)
 	guiSetVisible(windows["register"],false)
 	guiSetVisible(windows["recovery"],false)
 	showCursor(true)
-	loadCachedAccountData()
 end
 addEventHandler("onClientResourceStart",resourceRoot,onStart)
 
-function loadCachedAccountData()
-	cacheData = xmlLoadFile("@account.xml")
-	
-	if not cacheData then
-		cacheData = xmlCreateFile("@account.xml","account")
-		return false --Stop here, since we have no data to load
+function onClick(button,state)
+	if (button == "left") and (state == "up") then
+		--open section
+		if (source == buttons["login-troll"]) then
+			guiSetVisible(windows["NOPE"],true)
+			guiSetVisible(windows["login"],false)
+			playSound("sound/nope.mp3")
+		elseif (source == buttons["login-register"]) then
+			guiSetVisible(windows["register"],true)
+			guiSetVisible(windows["login"],false)
+		elseif (source == buttons["login-recover"]) then
+			guiSetVisible(windows["recovery"],true)
+			guiSetVisible(windows["login"],false)
+		
+		--close section
+		elseif (source == buttons["NOPE-fine"]) then
+			guiSetVisible(windows["NOPE"],false)
+			guiSetVisible(windows["login"],true)
+		elseif (source == buttons["register-back"]) then
+			guiSetVisible(windows["register"],false)
+			guiSetVisible(windows["login"],true)
+		elseif (source == buttons["recovery-back"]) then
+			guiSetVisible(windows["recovery"],false)
+			guiSetVisible(windows["login"],true)
+		
+		--attempt section
+		elseif (source == buttons["login-attemptLogin"]) then
+			outputChatBox("NOPE")
+			playSound("sound/nope.mp3") --We'll remove these later.
+		elseif (source == buttons["register-attemptRegister"]) then
+			outputChatBox("NOPE")
+			playSound("sound/nope.mp3") --We'll remove these later.
+		elseif (source == buttons["recovery-attemptRecovery"]) then
+			outputChatBox("NOPE")
+			playSound("sound/nope.mp3") --We'll remove these later.
+		else
+			outputChatBox("unknown button!")
+		end
 	end
-	
-	local username = xmlNodeGetAttribute(cacheData,"username") or "" --In case nothing's set, we'll just put an empty string in it's place.
-	local password = xmlNodeGetAttribute(cacheData,"password") or "" --Same for this.
-	local remember = xmlNodeGetAttribute(cacheData,"remember") or "false"
-	guiSetText(edits["login-username"],username)
-	guiSetText(edits["login-password"],password)
-	guiCheckBoxSetSelected(checkboxes["remember"],exports.utils:convertToBool(remember))
-	
-	return true
 end
 
-function onPlayerLoggedIn(password,remember)
-	if (remember) then
-		--Password's already encrypted.
+function onElementChanged(element)
+	if (element == edits["login-username"]) then
+		local username = guiGetText(edits["login-username"])
+		if (#username >= 1) then
+			guiSetText(buttons["login-attemptLogin"],"Log in as "..username)
+		else
+			guiSetText(buttons["login-attemptLogin"],"Login")
+		end
+	elseif (element == edits["register-username"]) then
+		local username = guiGetText(edits["register-username"])
+		if (#username >= 1) then
+			guiSetText(buttons["register-attemptRegister"],"Register as "..username)
+		else
+			guiSetText(buttons["register-attemptRegister"],"Register account")
+		end
+	elseif (element == edits["recovery-username"]) then
+		local username = guiGetText(edits["recovery-username"])
+		if (#username >= 1) then
+			guiSetText(buttons["recovery-attemptRecovery"],"Recover username "..username)
+		else
+			guiSetText(buttons["recovery-attemptRecovery"],"Recover account")
+		end
 	end
+end
+addEventHandler("onClientGUIChanged",root,onElementChanged)
+
+function onPlayerLoggedIn(username,password,remember)
+	if (remember) then
+		if not cacheData then
+			loadCachedAccountData()
+		end
+		
+		xmlNodeSetAttribute(cacheData,"username",username)
+		xmlNodeSetAttribute(cacheData,"password",password)
+		xmlNodeSetAttribute(cacheData,"remember","true")
+		xmlNodeSetAttribute(cacheData,"encrypted","true")
+	else
+		xmlNodeSetAttribute(cacheData,"username","")
+		xmlNodeSetAttribute(cacheData,"password","")
+		xmlNodeSetAttribute(cacheData,"remember","false")
+		xmlNodeSetAttribute(cacheData,"encrypted","false")
+	end
+	
+	--Check if he has a character, otherwise prompt him to the character creation
 end
 addEventHandler("onPlayerLoggedIn",root,onPlayerLoggedIn)
