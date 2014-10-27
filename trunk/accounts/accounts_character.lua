5e6d2d2aaedcb45560ffe74daf2f606232f9a611
britishjackj@gmail.com <britishjackj@gmail.com@9318ff57-b472-33fd-257b-0f0ced4443b5>
2014-10-27 01:37:06 +0000

    [Add] Characters can now spawn! (Jack) [Change] Scripts have been renamed to their type. (Jack) [Fix] Fixed an issue which prevented account data being loaded when character was being created. (Jack)

--- trunk/accounts/accounts_character.lua
+++ trunk/accounts/accounts_character.lua
@@ -8,6 +8,10 @@
 local windows = {}
 local gridlists = {}
 local buttons = {}
+local pedPosition = {1994.044921875, 1580.0517578125, 17.5625, 17}
+local characters --local copy of the player's characters
+local selectedChar --Currently selected character
+local character --ped
 
 addEvent("onCharacterDataLoaded",true)
 
@@ -30,7 +34,9 @@ function onStart()
 	buttons["play"] = guiCreateButton(X,Y+((Y/2)*1.6),width,height, "LET'S PARTY!", false)
 	
 	--button eventhandlers
-	addEventHandler("onClientGUIClick",buttons["logout"],onCharacterClickClick,false)
+	addEventHandler("onClientGUIClick",buttons["logout"],onCharacterClick,false)
+	addEventHandler("onClientGUIClick",buttons["play"],onCharacterClick,false)
+	addEventHandler("onClientGUIClick",gridlists["characters"],onCharacterClick,false)
 	
 	guiSetVisible(windows["characters"],false)
 	guiSetVisible(buttons["play"],false)
@@ -45,17 +51,24 @@ function loadCharacterData(data)
 	fadeCamera(false,1.0,0,0,0)
 	toggleWindows("all",false)
 	guiGridListClear(gridlists["characters"])
-	if (data) and (#data >= 1) then
+	if (data) then
 		local data = fromJSON(data) --change back to a table!
-		for k,v in ipairs(data) do
-			local name = v.charName
+		if (#data >= 1) then
+			for k,v in ipairs(data) do
+				local name = v.charName
+				local row = guiGridListAddRow(gridlists["characters"])
+				guiGridListSetItemText(gridlists["characters"],row,characters,name,false,false)
+			end
+		else
 			local row = guiGridListAddRow(gridlists["characters"])
-			guiGridListSetItemText(gridlists["characters"],row,characters,name,false,false)
+			guiGridListSetItemText(gridlists["characters"],row,characters,"You have no characters!",false,false)
 		end
 	else
 		local row = guiGridListAddRow(gridlists["characters"])
 		guiGridListSetItemText(gridlists["characters"],row,characters,"You have no characters!",false,false)
 	end
+	
+	characters = fromJSON(data) --Store this for later use.
 	setTimer(guiSetVisible,1000,1,windows["characters"],true)
 	setTimer(guiSetVisible,1000,1,buttons["play"],true)
 	setTimer(fadeCamera,1000,1,true,1.0)
@@ -63,12 +76,52 @@ function loadCharacterData(data)
 end
 addEventHandler("onCharacterDataLoaded",root,loadCharacterData)
 
-function onCharacterClickClick(button,state)
+function onCharacterClick(button,state)
 	if (button == "left" and state == "up") then
 		if (source == buttons["logout"]) then
 			triggerServerEvent("onPlayerAttemptLogout",localPlayer)
+		elseif (source == gridlists["characters"]) then
+			processCharacter()
+		elseif (source == buttons["play"]) then
+			if (character) then
+				triggerServerEvent("onPlayerSelectCharacter",localPlayer,character)
+				character = nil --Clear some memory for the client
+			else
+				return false --no idea how he gets passed this.
+			end
 		end
 	end
+end
+
+function processCharacter()
+	--First, figure out if we've got anything selected
+	local row,column = guiGridListGetSelectedItem(gridlists["characters"])
+	if (row ~= -1 and columns ~= 1) then
+		local charName = guiGridListGetItemText(gridlists["characters"],row,1)
+		if charName then
+			for k,v in ipairs(characters) do
+				if (v.charName == charName) then
+					character = v
+					break
+				end
+			end
+			
+			if (character) then
+				local skin = character.model or 0 
+				if ped and isElement(ped) then destroyElement(ped) end
+				ped = createPed(tonumber(skin),pedPosition[1],pedPosition[2],pedPosition[3],250)
+				setElementFrozen(ped,true)
+				setPedAnimation(ped,"DANCING","dance_loop",-1,true,false)
+				guiSetEnabled(buttons["play"],true)
+				return true
+			end
+		end
+	end
+	
+	if ped and isElement(ped) then destroyElement(ped) end
+	guiSetEnabled(buttons["play"],false)
+	character = nil
+	return false
 end
 
 function toggleCharacterWindows(window,state)
