b31dfab9da03033cf8c62c97d02087cf89aba2a1
britishjackj@gmail.com <britishjackj@gmail.com@9318ff57-b472-33fd-257b-0f0ced4443b5>
2014-11-03 23:40:30 +0000

    [Add] Added new greenzones to 2 hospitals. More pending. [Tweak] Added SourceMode logo to accounts. [Update] Rewrote interiors to be more cleaner.
    [PLANNED] Read Planned.txt for more information about our versions and a upcoming feature.

--- trunk/club/interior.lua
+++ trunk/club/interior.lua
@@ -1,61 +1,50 @@
-local entranceDiscoMarker = createMarker(1960, 1560.45, 11, "arrow", 1.5, 255, 20, 147, 150)
-function onDiscoIntEnter(hitBy, dimensionMatch)
-	if dimensionMatch and getElementType(hitBy) == "player" and not isPedInVehicle(hitBy) then
-		setPedFrozen(hitBy, true)
-		setTimer(setPedFrozen, 1000, 1, hitBy, false)
-		setTimer(setElementInterior, 1000, 1, hitBy, 1)
-		setTimer(setElementDimension, 1000, 1, hitBy, 1000)
-		setTimer(setElementPosition, 1000, 1, hitBy, 1948.22, 1576.5, -46.71)
-		setTimer(setPedRotation, 1000, 1, hitBy, 180)
-		fadeCamera(hitBy, false, 1, 0, 0, 0)
-		setTimer(fadeCamera, 1000, 1, hitBy, true, 1)
-	end
-end
-addEventHandler("onMarkerHit", entranceDiscoMarker, onDiscoIntEnter)
+local interiors = {
+{marker = {1960,1560.45,11,0,0}, coords = {1948.22,1576,-46.71,1,1000, 180}}, --Club Enterance
+{marker = {1948,1578.65,-46,1,1000}, coords = {1960,1558.3,10.45,0,0, 180}}, --Club Exit
+{marker = {1937,1566,-46,1,1000}, coords = {1926,1563,-46.71,1,1000, 180}}, --Toilet Enterance
+{marker = {1926,1565,-46,1,1000}, coords = {1939.84, 1566, -46.71,1,1000, 180}} --Toilet Exit
+}
+local markers = {}
 
-local exitDiscoMarker = createMarker(1948.22, 1578.65, -46, "arrow", 1.5, 255, 20, 147, 150)
-setElementInterior(exitDiscoMarker, 1)
-setElementDimension(exitDiscoMarker, 1000)
-function onDiscoIntLeave(hitBy, dimensionMatch)
-	if dimensionMatch and getElementType(hitBy) == "player" and not isPedInVehicle(hitBy) then
-		setPedFrozen(hitBy, true)
-		setTimer(setPedFrozen, 1000, 1, hitBy, false)
-		setTimer(setElementInterior, 1000, 1, hitBy, 0)
-		setTimer(setElementDimension, 1000, 1, hitBy, 0)
-		setTimer(setElementPosition, 1000, 1, hitBy, 1960, 1558.3, 10.45)
-		setTimer(setPedRotation, 1000, 1, hitBy, 180)
-		fadeCamera(hitBy, false, 1, 0, 0, 0)
-		setTimer(fadeCamera, 1000, 1, hitBy, true, 1)
+function onStart()
+	for k,v in pairs(interiors) do
+		v = interiors[k]
+		local marker = createMarker(v.marker[1],v.marker[2],v.marker[3]-1.5,"cylinder",1.5,255,27,147,150)
+		setElementInterior(marker,v.marker[4])
+		setElementDimension(marker,v.marker[5])
+		markers[marker] = {index = k}
+	end
+	
+	--TEST--
+	if (fileExists("club.map")) then
+		outputConsole("Map found.")
+	else
+		outputConsole("Map not found.")
 	end
 end
-addEventHandler("onMarkerHit", exitDiscoMarker, onDiscoIntLeave)
+addEventHandler("onResourceStart",resourceRoot,onStart)
 
-local entranceDiscoToiletMarker = createMarker(1937.84, 1566, -46, "arrow", 1.5, 255, 20, 147, 150)
-setElementInterior(entranceDiscoToiletMarker, 1)
-setElementDimension(entranceDiscoToiletMarker, 1000)
-function onDiscoToiletEnter(hitBy, dimensionMatch)
-	if dimensionMatch and getElementType(hitBy) == "player" and not isPedInVehicle(hitBy) then
-		setPedFrozen(hitBy, true)
-		setTimer(setPedFrozen, 1000, 1, hitBy, false)
-		setTimer(setElementPosition, 1000, 1, hitBy, 1926.62, 1563.6, -46.71)
-		setTimer(setPedRotation, 1000, 1, hitBy, 180)
-		fadeCamera(hitBy, false, 1, 0, 0, 0)
-		setTimer(fadeCamera, 1000, 1, hitBy, true, 1)
+function onHit(hitEl, md)
+	if not source then return false end
+	if not hitEl or not isElement(hitEl) or not getElementType(hitEl) == "player" then return false end
+	
+	if markers[source] then
+		local index = markers[source].index
+		local v = interiors[index]
+		
+		local x,y,z = v.coords[1],v.coords[2],v.coords[3]
+		local int = v.coords[4]
+		local dim = v.coords[5]
+		local rot = v.coords[6]
+		
+		setTimer(
+			function(player,x,y,z,int,dim, rot)
+				setElementPosition(player,x,y,z)
+				setElementInterior(player,int)
+				setElementDimension(player,dim)
+				setElementRotation(player,0,0,rot)
+			end, 1000, 1, hitEl, x, y, z, int, dim, rot
+		)
 	end
 end
-addEventHandler("onMarkerHit", entranceDiscoToiletMarker, onDiscoToiletEnter)
-
-local exitDiscoToiletMarker = createMarker(1926.62, 1565.6, -46, "arrow", 1.5, 255, 20, 147, 150)
-setElementInterior(exitDiscoToiletMarker, 1)
-setElementDimension(exitDiscoToiletMarker, 1000)
-function onDiscoToiletLeave(hitBy, dimensionMatch)
-	if dimensionMatch and getElementType(hitBy) == "player" and not isPedInVehicle(hitBy) then
-		setPedFrozen(hitBy, true)
-		setTimer(setPedFrozen, 1000, 1, hitBy, false)
-		setTimer(setElementPosition, 1000, 1, hitBy, 1939.84, 1566, -46.71)
-		setTimer(setPedRotation, 1000, 1, hitBy, 270)
-		fadeCamera(hitBy, false, 1, 0, 0, 0)
-		setTimer(fadeCamera, 1000, 1, hitBy, true, 1)
-	end
-end
-addEventHandler("onMarkerHit", exitDiscoToiletMarker, onDiscoToiletLeave)
+addEventHandler("onMarkerHit",root,onHit)
