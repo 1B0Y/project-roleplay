6c9c816deb3be82c17959e17e3d961a8da574476
britishjackj@gmail.com <britishjackj@gmail.com@9318ff57-b472-33fd-257b-0f0ced4443b5>
2014-11-05 03:33:36 +0000

    [Add] Added "nogoto" which will allow players to teleport to their friends. [Add] Added a new "info" section on the player's screen which will display their bank balance and the newly added "nogoto" feature. [Add] Added "convertToMoney", "findPlayer" and "getDateAndTime" exports in utils. [Tweak] Adjusted accounts_database to accept player elements as well as their usernames.

--- trunk/player/hud.lua
+++ trunk/player/hud.lua
@@ -15,6 +15,7 @@ function onStart() --This is triggered when the resource has been executed
 	end
 	
 	addEventHandler("onClientRender",root,logo)
+	addEventHandler("onClientRender",root,info) --Bank, Nogoto
 end
 addEventHandler("onClientResourceStart",resourceRoot,onStart)
 
@@ -29,8 +30,37 @@ function resWarning()
 end
 
 function logo()
-	local width,height = 350,145
-	local angle = math.sin(getTickCount() / 1000) * 3.5
+	--if not (getResourceState(getResourceFromName("accounts")) == "running") then return false end
+	if not (exports.accounts:isPlayerLoggedIn()) or not (exports.accounts:isPlayerSpawned()) then return false end
+	local scale = .75
+	local width,height = 350*scale,145*scale
+	local speed = 5000 --Increase to go slower, decrease to go faster.
+	local angle = math.cos(getTickCount() / speed) * 5
 	
-	dxDrawImage(rX-width+10,rY-height+25,width,height,"images/sourcemode.png",angle,0,0,tocolor(255,255,255,125),false)
+	dxDrawImage(rX/1.15-width/2,rY/1.1-height/2,width,height,"images/sourcemode.png",angle,0,0,tocolor(255,255,255,175),false)
+end
+
+function info()
+	if not (exports.accounts:isPlayerLoggedIn()) or not (exports.accounts:isPlayerSpawned()) then return false end
+	
+	local bankMoney = 999999999 --Change this to the export later
+	local nogoto = getElementData(localPlayer,"nogoto")
+	local font = "pricedown"
+	
+	--hud = "Bank: #00FF00$"..exports.utils:convertToMoney(bankMoney).."\n" --Start off with drawing the Bank hud
+	hud = ""
+	
+	--Process it more by adding the Nogoto state
+	if nogoto == true then
+		hud = hud .. "#FFFFFFNogoto: #7cbb00Enabled"
+	elseif nogoto == false then
+		hud = hud .. "#FFFFFFNogoto: #f65314Disabled"
+	end
+	
+	local width,height = dxGetTextWidth(hud:gsub("#%x%x%x%x%x%x",""),1,font), dxGetFontHeight(1,font)
+	
+	--Thank you ccw for debugging this part.
+	local x,y,w,h = rX/1.17-width/2, rY/3.3-height/2, width, height
+	dxDrawText(hud,x,y,x+w,y+h,tocolor(255,255,255,255),1,font,"center", "center", false, false, false, true, true)
+	--dxDrawRectangle(x,y,w,h,tocolor(255,255,255,128))
 end
--- trunk/utils/utils.lua
+++ trunk/utils/utils.lua
@@ -53,3 +53,83 @@ function rgbToHex(r,g,b,a)
 		return string.format("#%.2X%.2X%.2X", r,g,b)
 	end
 end
+
+function convertToMoney( theNumber )  
+	local formatted = theNumber  
+	while true do      
+		formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')    
+	if (k==0) then      
+		break   
+		end  
+	end  
+	return formatted
+end
+
+function findPlayer( namePart )
+	local result = false
+    if namePart then
+        for i, player in ipairs(getElementsByType("player")) do
+            if string.find(getPlayerName(player):lower(), tostring(namePart):lower(), 1, true) then
+				if result then return false end
+					result = player 
+				end
+			end
+		end
+    return result
+end
+
+function findPlayer(name)
+	local player
+	if name and #name >= 1 then
+		for k,v in ipairs(getElementsByType("player")) do
+			if string.find(getPlayerName(v):lower(), tostring(name):lower(), 1, true) then
+				player = v
+				break
+			end
+		end
+	end
+	
+	return player
+end
+
+--Dang, this is a big function :O
+function getDateAndTime()
+	local now = getRealTime()
+	
+	--Do some extra work for hours, minutes and etc.
+	if (now.month >= 0) and (now.month < 10) then
+		month = "0"..now.month
+	else
+		month = now.month
+	end
+	
+	if (now.monthday+1 >= 0) and (now.monthday < 10) then
+		day = "0"..now.monthday
+	else
+		day = now.monthday
+	end
+	
+	if (now.hour >= 0) and (now.hour < 10) then
+		hour = "0"..now.hour
+	else
+		hour = now.hour
+	end
+	
+	if (now.minute >= 0) and (now.minute < 10) then
+		minute = "0"..now.minute
+	else
+		minute = now.minute
+	end
+	
+	if (now.second >= 0) and (now.second < 10) then
+		second = "0"..now.second
+	else
+		second = now.second
+	end
+	
+	local year,month,day = 1900+now.year,month,day
+	
+	local dat = year.."-"..month.."-"..day.." "..hour..":"..minute..":"..second
+	
+	return dat
+end
